faec862b858759fabd4afa4565b51d36da050338
Brain Wonderson <112601710+brainwonderson@users.noreply.github.com>
2026-07-29 08:43:18 +0700

    feat: tambahkan favicon logo ecomart premium ke tab browser

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles.css'
 import Script from 'next/script'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <title>EcoMart – Belanja Cerdas, Lebih Cepat</title>
+      </Head>
       {/* Midtrans Snap JS — Sandbox */}
       <Script
         src="https://app.sandbox.midtrans.com/snap/snap.js"
